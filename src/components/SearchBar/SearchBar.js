2f5fd03f53b404ab2dc1efd3a70c41d16e5a68e5
sergANTstar <106014778+sergANTstar@users.noreply.github.com>
2022-08-19 11:16:41 +0300

    u

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import css from './Searchbar.module.css';
+import css from './SearchBar.module.css';
 import { ReactComponent as SearchIcon } from './search.svg';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
